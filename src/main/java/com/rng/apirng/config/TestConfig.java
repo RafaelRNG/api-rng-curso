deab14753edc513b1c7e44ac32d2971dd2b6ad01
RafaelRNG <rafael.gomila@hotmail.com>
2020-11-22 03:06:28 -0300

    Criando profile de teste

---
+++ src/main/java/com/rng/apirng/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.rng.apirng.config;
+
+import com.rng.apirng.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public Boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
