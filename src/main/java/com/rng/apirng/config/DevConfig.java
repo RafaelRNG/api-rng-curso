c01f81de0a1b3c10b651ea4be259ab6b71962b8b
RafaelRNG <rafael.gomila@hotmail.com>
2020-11-27 03:11:59 -0300

    Implementando SmtpEmailService com Smtp do Google

--- src/main/java/com/rng/apirng/config/DevConfig.java
+++ src/main/java/com/rng/apirng/config/DevConfig.java
@@ -1,6 +1,8 @@
 package com.rng.apirng.config;
 
 import com.rng.apirng.services.DBService;
+import com.rng.apirng.services.EmailService;
+import com.rng.apirng.services.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -25,5 +27,10 @@ public class DevConfig {
 
         dbService.instantiateTestDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService emailService(){
+        return new SmtpEmailService();
     }
 }
