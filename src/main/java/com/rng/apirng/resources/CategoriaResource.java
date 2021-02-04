5a23b79504a33a5ca21bc729980a3df5d9898ad3
RafaelRNG <rafael.gomila@hotmail.com>
2021-02-04 15:25:47 -0300

    Liberando o CORS para PUT e DELETE

--- src/main/java/com/rng/apirng/resources/CategoriaResource.java
+++ src/main/java/com/rng/apirng/resources/CategoriaResource.java
@@ -1,22 +1,28 @@
 package com.rng.apirng.resources;
 
-import com.rng.apirng.dto.CategoriaDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import com.rng.apirng.domain.Categoria;
-import com.rng.apirng.services.CategoriaService;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.rng.apirng.domain.Categoria;
+import com.rng.apirng.dto.CategoriaDTO;
+import com.rng.apirng.services.CategoriaService;
 
 @RestController
 @RequestMapping(path = "/categorias")
